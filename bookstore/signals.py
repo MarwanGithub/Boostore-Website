84908dc21023239076268aff6fa39f666e337a70
MarwanMohamed-CS <78783656+MarwanMohamed-CS@users.noreply.github.com>
2025-06-21 22:27:11 +0300

    now whitenoise working correctly after we changed that line in last commit. in this commit we changes the signals to handle deleting file with cloudinary instead of handling it as if they are local files, this used to give error when deleting added books.

--- bookstore/signals.py
+++ bookstore/signals.py
@@ -1,21 +1,19 @@
 from django.db.models.signals import pre_save, post_delete
 from django.dispatch import receiver
 from .models import Book, BookImage
-import os
 
 @receiver(pre_save, sender=Book)
 def delete_old_book_cover_on_update(sender, instance, **kwargs):
     """
-    Deletes the old cover image file when a Book's cover_image is updated.
+    Deletes the old cover image file from Cloudinary when a Book's cover_image is updated.
     """
     if not instance.pk:
-        return False
+        return
 
     try:
         old_instance = sender.objects.get(pk=instance.pk)
         if old_instance.cover_image and instance.cover_image != old_instance.cover_image:
-            if os.path.isfile(old_instance.cover_image.path):
-                os.remove(old_instance.cover_image.path)
+            old_instance.cover_image.delete(save=False)
     except sender.DoesNotExist:
         # This happens when creating a new object.
         pass
@@ -23,34 +21,30 @@ def delete_old_book_cover_on_update(sender, instance, **kwargs):
 @receiver(post_delete, sender=Book)
 def delete_book_cover_on_delete(sender, instance, **kwargs):
     """
-    Deletes the cover image file when a Book is deleted.
+    Deletes the cover image file from Cloudinary when a Book is deleted.
     """
     if instance.cover_image:
-        if os.path.isfile(instance.cover_image.path):
-            os.remove(instance.cover_image.path)
-
+        instance.cover_image.delete(save=False)
 
 @receiver(pre_save, sender=BookImage)
 def delete_old_book_image_on_update(sender, instance, **kwargs):
     """
-    Deletes the old image file when a BookImage's image is updated.
+    Deletes the old image file from Cloudinary when a BookImage's image is updated.
     """
     if not instance.pk:
-        return False
+        return
 
     try:
         old_instance = sender.objects.get(pk=instance.pk)
         if old_instance.image and instance.image != old_instance.image:
-            if os.path.isfile(old_instance.image.path):
-                os.remove(old_instance.image.path)
+            old_instance.image.delete(save=False)
     except sender.DoesNotExist:
         pass
 
 @receiver(post_delete, sender=BookImage)
 def delete_book_image_on_delete(sender, instance, **kwargs):
     """
-    Deletes the image file when a BookImage is deleted.
+    Deletes the image file from Cloudinary when a BookImage is deleted.
     """
     if instance.image:
-        if os.path.isfile(instance.image.path):
-            os.remove(instance.image.path) 
+        instance.image.delete(save=False) 
