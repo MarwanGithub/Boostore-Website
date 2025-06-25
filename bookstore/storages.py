9132dbed3b304e0425acae5c28823420ec43b4f1
MarwanMohamed-CS <78783656+MarwanMohamed-CS@users.noreply.github.com>
2025-06-25 23:18:03 +0300

    back-end: changed number of parameters again in custom hash class to fix problem in production build. will test but previous commit doesn't work.

--- bookstore/storages.py
+++ bookstore/storages.py
@@ -1,35 +1,13 @@
 from cloudinary_storage.storage import StaticHashedCloudinaryStorage
-from django.contrib.staticfiles.storage import ManifestStaticFilesStorage
 
 class CustomStaticHashedCloudinaryStorage(StaticHashedCloudinaryStorage):
     """
     A custom storage class for Cloudinary that ignores missing source map files
     and other file-not-found errors during the collectstatic process.
     """
-    def url_converter(self, name):
+    def url_converter(self, name, MAPPING_URL_REGEXP, template):
         # Get the default converter from the parent class.
-        base_converter = super().url_converter(name)
-
-        def converter(matchobj):
-            try:
-                # Try to process the URL as usual.
-                return base_converter(matchobj)
-            except (ValueError, TypeError):
-                # If a ValueError is raised (e.g., file not found for a source map),
-                # or a TypeError (e.g., path is None due to jazzmin conflicts),
-                # just return the original, unprocessed URL.
-                return matchobj.group(0)
-
-        return converter
-
-class CustomManifestStaticFilesStorage(ManifestStaticFilesStorage):
-    """
-    A custom storage class for local development that ignores missing source map files
-    and other file-not-found errors during the collectstatic process.
-    """
-    def url_converter(self, name):
-        # Get the default converter from the parent class.
-        base_converter = super().url_converter(name)
+        base_converter = super().url_converter(name, MAPPING_URL_REGEXP, template)
 
         def converter(matchobj):
             try:
